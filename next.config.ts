814af55f8d750774f095640ff9f6cc0910af6515
MasAris1 <aris.maulana.am57@gmail.com>
2026-05-08 08:10:56 +0700

    Allow Midtrans Snap asset CDN

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const contentSecurityPolicy = [
   "default-src 'self'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://app.midtrans.com https://app.sandbox.midtrans.com https://*.midtrans.com",
-  "frame-src 'self' https://app.midtrans.com https://app.sandbox.midtrans.com https://*.midtrans.com",
-  "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://app.midtrans.com https://app.sandbox.midtrans.com https://*.midtrans.com",
-  "img-src 'self' data: blob: https://*.supabase.co https://images.unsplash.com",
-  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-  "font-src 'self' data: https://fonts.gstatic.com",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://app.midtrans.com https://app.sandbox.midtrans.com https://*.midtrans.com https://*.gtflabs.io",
+  "frame-src 'self' https://app.midtrans.com https://app.sandbox.midtrans.com https://*.midtrans.com https://*.gtflabs.io",
+  "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://app.midtrans.com https://app.sandbox.midtrans.com https://*.midtrans.com https://*.gtflabs.io",
+  "img-src 'self' data: blob: https://*.supabase.co https://images.unsplash.com https://*.gtflabs.io",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.gtflabs.io",
+  "font-src 'self' data: https://fonts.gstatic.com https://*.gtflabs.io",
 ].join("; ");
 
 const nextConfig: NextConfig = {
